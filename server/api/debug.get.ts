0c91b2aff9586cd0c2b491e16d663f98c6d614c5
Daniel Nemetz <daniel@nemetz.de>
2026-03-14 12:33:16 +0100

    Fix Docker environment variable loading in deployment

--- server/api/debug.get.ts
+++ server/api/debug.get.ts
@@ -1,7 +1,7 @@
 export default defineEventHandler(() => {
   return {
-    version: '1.0.6',
-    buildTime: '2026-03-14 12:28',
+    version: '1.0.7',
+    buildTime: '2026-03-14 12:34',
     status: 'online'
   };
 });
