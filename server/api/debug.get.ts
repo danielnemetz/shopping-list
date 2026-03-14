00bd3efff4cfa54a48d2a3d087cb6e0b18a7c361
Daniel Nemetz <daniel@nemetz.de>
2026-03-14 12:25:30 +0100

    Force Docker build --no-cache and add deep diagnostics

--- server/api/debug.get.ts
+++ server/api/debug.get.ts
@@ -1,7 +1,7 @@
 export default defineEventHandler(() => {
   return {
-    version: '1.0.5',
-    buildTime: '2026-03-14 12:22',
+    version: '1.0.6',
+    buildTime: '2026-03-14 12:28',
     status: 'online'
   };
 });
