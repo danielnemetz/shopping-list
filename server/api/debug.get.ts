b21cbc2552f15318d490f5fb7071cf0623937050
Daniel Nemetz <daniel@nemetz.de>
2026-03-14 12:17:51 +0100

    Fix SCP source pattern by explicitly listing folders

--- server/api/debug.get.ts
+++ server/api/debug.get.ts
@@ -1,7 +1,7 @@
 export default defineEventHandler(() => {
   return {
-    version: '1.0.3',
-    buildTime: '2026-03-14 12:12',
+    version: '1.0.4',
+    buildTime: '2026-03-14 12:18',
     status: 'online'
   };
 });
