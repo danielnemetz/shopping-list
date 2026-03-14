3d91c80a9114170d87961720eb7a34c5ea1b48c6
Daniel Nemetz <daniel@nemetz.de>
2026-03-14 12:21:56 +0100

    Switch to rsync-based deployment for high fidelity sync

--- server/api/debug.get.ts
+++ server/api/debug.get.ts
@@ -1,7 +1,7 @@
 export default defineEventHandler(() => {
   return {
-    version: '1.0.4',
-    buildTime: '2026-03-14 12:18',
+    version: '1.0.5',
+    buildTime: '2026-03-14 12:22',
     status: 'online'
   };
 });
