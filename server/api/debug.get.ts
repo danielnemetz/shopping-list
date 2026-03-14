af985bacbce3e184b58e43edb6fe8410a50d4d13
Daniel Nemetz <daniel@nemetz.de>
2026-03-14 12:10:00 +0100

    Switch to SCP push deployment

--- server/api/debug.get.ts
+++ server/api/debug.get.ts
@@ -1,7 +1,7 @@
 export default defineEventHandler(() => {
   return {
-    version: '1.0.1',
-    buildTime: '2026-03-14 12:05',
+    version: '1.0.2',
+    buildTime: '2026-03-14 12:10',
     status: 'online'
   };
 });
