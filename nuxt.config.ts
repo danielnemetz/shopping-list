d3f418eee1a9310a540115e37ce6090fd743e285
Daniel Nemetz <daniel@nemetz.de>
2026-03-15 10:40:39 +0100

    Fix manifest

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -35,16 +35,23 @@ export default defineNuxtConfig({
       categories: ['shopping', 'productivity'],
       icons: [
         {
-          src: '/icon-512.png',
-          sizes: '512x512',
-          type: 'image/png',
-          purpose: 'any maskable',
-        },
-        {
           src: '/icon-192.png',
           sizes: '192x192',
           type: 'image/png',
-        }
+          purpose: 'any',
+        },
+        {
+          src: '/icon-512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'any',
+        },
+        {
+          src: '/icon-512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'maskable',
+        },
       ],
     },
     workbox: {
